0ef56722dd5f25d31cdae689dd624709befabee1
juanjborja <juan.borja@patagoniansys.com>
2020-11-21 21:04:03 -0300

    Added dynamic pagination

--- src/js/api.js
+++ src/js/api.js
@@ -2,8 +2,7 @@ const BASE_URL = 'http://localhost:7000';
 
 export const getStocks = async (page, size, fields) => {
   try {
-    return (await axios.get(`${BASE_URL}/api/stocks?page=${page}&size=${size}&fields=${fields}`))
-      ?.data;
+    return await axios.get(`${BASE_URL}/api/stocks?page=${page}&size=${size}&fields=${fields}`);
   } catch (e) {
     console.error(e);
   }
--- src/js/render.js
+++ src/js/render.js
@@ -1,5 +1,8 @@
 import { getStocks } from './api.js';
 
+let page = 0;
+let size = 10;
+let pages = 0;
 const createRow = (item) => {
   const tr = document.createElement('tr');
   for (let [key, value] of Object.entries(item)) {
@@ -15,13 +18,13 @@ const createRow = (item) => {
     }
 
     tr.appendChild(td1);
-    console.log(key, value);
   }
   return tr;
 };
 
 const addStocksToRecord = (stocks) => {
-  const tableBody = document.querySelector('tbody');
+  const tableBody = document.getElementById('tableRecordBody');
+  tableBody.textContent = '';
   if (Array.isArray(stocks.rows) && stocks.rows.length > 0) {
     stocks.rows.map((stock) => {
       tableBody.appendChild(createRow(stock));
@@ -29,8 +32,60 @@ const addStocksToRecord = (stocks) => {
   }
 };
 
+const createPagination = (count) => {
+  const paginator = document.getElementById('paginator');
+  paginator.textContent = '';
+  pages = Math.ceil(count / 10);
+  for (let i = 0; i < pages; i++) {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'page-item');
+    const a = document.createElement('a');
+    a.setAttribute('class', 'page-link');
+    a.setAttribute('href', '#');
+    a.setAttribute('id', (i + 1).toString());
+    a.appendChild(document.createTextNode((i + 1).toString()));
+    li.appendChild(a);
+    paginator.appendChild(li);
+  }
+};
+const apiWrapper = async (page, size) => {
+  const result = await getStocks(page, size, 'id,min,vol,max');
+  createPagination(result?.data.count);
+  addStocksToRecord(result?.data);
+};
+const shouldBlockPreviousNext = (page) => {
+  const previous = document.getElementById('previousLi');
+  page === 0
+    ? previous.setAttribute('class', 'page-item disabled')
+    : previous.setAttribute('class', 'page-item');
+
+  const next = document.getElementById('NextLi');
+  page === pages - 1
+    ? next.setAttribute('class', 'page-item disabled')
+    : next.setAttribute('class', 'page-item');
+};
 const main = async () => {
-  addStocksToRecord(await getStocks(0, 20, 'id,min,vol,max'));
+  apiWrapper(page, size);
+  shouldBlockPreviousNext(page);
+  window.addEventListener('click', (event) => {
+    switch (event.target.id) {
+      case 'next':
+        page = page + 1;
+        shouldBlockPreviousNext(page);
+        break;
+      case 'previous':
+        page = page - 1;
+        shouldBlockPreviousNext(page);
+        break;
+      default:
+        const pageAux = Number(event.target.id);
+        console.log(pageAux);
+        page = pageAux - 1;
+        shouldBlockPreviousNext(page);
+        break;
+    }
+    apiWrapper(page, size);
+  });
 };
 
 main();
